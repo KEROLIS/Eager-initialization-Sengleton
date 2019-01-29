4465b8d318fcad4179c6c78976f35ce04938a52a
KEROLIS <kerlisyoussif12@gmail.com>
2019-01-29 19:37:37 +0200

    Add files via upload

--- Sengleton.java
+++ Sengleton.java
@@ -1,10 +1,15 @@
 
 //lazy Sengleton
 public class Sengleton {
-    static private  Sengleton inst = new Sengleton() ;
-    
-     private  Sengleton() {}
-     
-   static Sengleton getInest(){return inst;}
-}
+  static private Sengleton inst;
 
+  private Sengleton() {
+  }
+
+  static Sengleton getInest() {
+    if (inst==null) {
+      inst=new Sengleton();
+    }
+    return inst;
+  }
+}
